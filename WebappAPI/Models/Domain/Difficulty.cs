f21d4aea33f6ee595edc8008f434b93d0e50118f
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-06-30 18:23:55 +0200

    Add Difficulty and Region models

---
+++ WebappAPI/Models/Domain/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace WebappAPI.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+   
+    }
+}
---
+++ WebappAPI/Models/Domain/Region.cs
@@ -0,0 +1,8 @@
+﻿namespace WebappAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
