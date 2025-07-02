fd3b711916e0054bac5f3bcb109d293c3c28e331
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-07-02 16:10:41 +0200

    Add the DbContext class, add the connection string to the appsetting.json file, installed Entity Framework and its tools.

---
+++ WebappAPI/Data/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebappAPI.Models.Domain;
+
+// This creates the 3 tables I need in SQL Server with Entity Framework
+namespace WebappAPI.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
