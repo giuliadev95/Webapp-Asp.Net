bb986c399061f6f9a7952f5d964cfc675e502b2a
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-07-02 17:08:09 +0200

    Add SQL Server dependency injection

--- WebappAPI/Program.cs
+++ WebappAPI/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WebappAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DBContext>(
+    options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebappAPI"))
+ );
+
 
 var app = builder.Build();
 
