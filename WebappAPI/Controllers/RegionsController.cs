90d31236740a4b15684954c79a2f03e3c31971f6
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-07-11 23:04:13 +0200

    Add sample Region Controller

---
+++ WebappAPI/Controllers/RegionsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebappAPI.Controllers
+{
+        // https://localhost:portnumber/api/Students
+        [Route("api/controller")]
+        [ApiController]
+        public class RegionsController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Index()
+            {
+                string[] regions = new string[] { "String example" };
+                return Ok(regions);
+            }
+
+        }
+}
